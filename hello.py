678dca1f0d2a1171e85677e66304a65b23ae74a8
jagomez23 <u0460391@umail.utah.edu>
2021-09-14 09:48:28 -0600

    changes

---
+++ hello.py
@@ -0,0 +1,25 @@
+x = 1
+y = x + 5
+z = y
+
+x = 1
+x = 1.0
+
+x = 1
+print(type(x))
+
+x = 1.0
+print(type(x))
+
+x = True
+print(type(x))
+
+x = "This is a string"
+print(x)
+print(type(x))
+y = "This is also a string"
+
+x = 'Hello' + '' + 'World!'
+print(x)
+
+print('Hello World!')
